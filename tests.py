6a7e4648ed8d3dd1bc7141055c1d38119f63afdf
Rob Luke <code@robertluke.net>
2024-11-10 21:04:03 +1100

    Create tests.py

---
+++ tests.py
@@ -0,0 +1,8 @@
+def sum_activation(a):
+  """Returns the sum of the values."""
+  return sum(a)
+
+def test_calculate_sum_activation():
+  data = [1, 2, 3]
+  result = sum_activation(data)
+  assert result == 6
