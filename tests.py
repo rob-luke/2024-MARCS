b5ee40d8a550cd96654759ff4a5ea7f7d2897bca
Rob Luke <code@robertluke.net>
2024-11-10 21:07:00 +1100

    Update tests.py

--- tests.py
+++ tests.py
@@ -6,3 +6,8 @@ def test_calculate_sum_activation():
   data = [1, 2, 3]
   result = sum_activation(data)
   assert result == 6
+
+def test_calculate_sum_activation_fails():
+  data = [1, 2, 3]
+  result = sum_activation(data)
+  assert result == 5
